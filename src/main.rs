b2a1aac57348a904f6da97fe5c6bccb35c6ee3c7
Moritz Breipohl <mbreipohl@techfak.de>
2023-01-16 23:35:24 +0100

    Moved functionality testing from main to tests/

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,6 @@
-mod common;
-mod test_runner;
-
-use crate::common::*;
-use crate::test_runner::*;
+use ttt::prelude::*;
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    let instructions = vec![
-        Instruction::LaunchProcess("uname".into(), 0),
-        Instruction::PutStdin("wololo".into(), 0),
-        Instruction::ExpectStdout("Linux".into(), 0),
-        Instruction::ExpectExitCode(0, 0),
-        Instruction::LaunchProcess("uname".into(), 1),
-        Instruction::ExpectRegex(r"(l|L)inux".into(), 1),
-        Instruction::SendControlCharacter('C', 1),
-        Instruction::ExpectExitCode(0, 1),
-    ];
-
-    let test_case = SingleTest {
-        name: "Beispiel".into(),
-        instructions,
-    };
-
-    let runner = TestRunner::new(test_case);
-    runner.run()?;
-    println!("Test ran successfully!");
     Ok(())
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -18,3 +18,46 @@ fn sample_test() -> Result<(), TestRunnerError> {
 
     Ok(())
 }
+
+#[test]
+fn use_each_instruction() -> Result<(), TestRunnerError> {
+    let instructions = vec![
+        Instruction::LaunchProcess("uname".into(), 0),
+        Instruction::PutStdin("wololo".into(), 0),
+        Instruction::ExpectStdout("Linux".into(), 0),
+        Instruction::ExpectExitCode(0, 0),
+        Instruction::LaunchProcess("uname".into(), 1),
+        Instruction::ExpectRegex(r"(l|L)inux".into(), 1),
+        Instruction::SendControlCharacter('C', 1),
+        Instruction::ExpectExitCode(0, 1),
+    ];
+
+    let test_case = SingleTest {
+        name: "Beispiel".into(),
+        instructions,
+    };
+
+    let runner = TestRunner::new(test_case);
+    runner.run()?;
+
+    Ok(())
+}
+
+#[test]
+#[should_panic]
+fn verify_control_character() {
+    let instructions = vec![
+        Instruction::LaunchProcess("sleep 100".into(), 0),
+        Instruction::SendControlCharacter('C', 0),
+        Instruction::ExpectExitCode(0, 0)
+
+    ];
+
+    let test_case = SingleTest {
+        name: "Beispiel".into(),
+        instructions,
+    };
+
+    let runner = TestRunner::new(test_case);
+    runner.run().unwrap();
+}
