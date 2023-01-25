880a17aeab6c10cb5e40d6a14ad3f03f102bad68
Moritz Breipohl <mbreipohl@techfak.de>
2023-01-25 20:47:12 +0100

    Cleaned up main

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ mod test_parser;
 mod test_runner;
 
 use anyhow::Result;
-use std::fs;
-use test_parser::parse;
+use std::{fs, collections::HashMap};
+use test_parser::{parse, print_tree};
 use test_runner::TestSuiteRunner;
 
 //extern crate pest_derive;
@@ -13,7 +13,6 @@ fn main() -> Result<()> {
     let test_file_data = fs::read_to_string("sample.test")?;
 
     let test_suite = parse(&test_file_data)?;
-    //println!("{test_suite:#?}");
 
     let runner = TestSuiteRunner::new(test_suite);
     runner.run();
