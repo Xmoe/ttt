32b92e38d9b2339487c459930d0d7e49566e9e77
Moritz Breipohl <mbreipohl@techfak.de>
2023-04-28 11:20:48 +0200

    added color output and requirements

--- ttt_formatter.py
+++ ttt_formatter.py
@@ -2,17 +2,25 @@
 
 from dataclasses import dataclass
 from ttt_common import TestFailed
+from pathlib import Path
+from colorama import Back
+
 
 @dataclass
 class TestResults:
     program: str
-    results: list[None|TestFailed]
+    results: list[None | TestFailed]
 
     def display(self):
         failures = [x for x in self.results if x is not None]
-        successes = len(self.results) - len(failures)
-        print(f"\n[SUMMARY] [{successes:2}/{len(self.results):2}] {self.program}")
+        num_successes = len(self.results) - len(failures)
+
+        bg_color = Back.GREEN if len(
+            failures) == 0 else Back.RED
+
+        print(
+            f"\n{bg_color}[SUMMARY] [{num_successes:2}/{len(self.results):2}] {Path(self.program).stem}{Back.RESET}")
         for failure in failures:
-            print(f"Test '{failure.test_name}' at line {failure.line_number}:")
-            print(f"  Expected: {failure.expected_value}")
-            print(f"  Actual:   {failure.actual_value}")
+            print(f"{Back.LIGHTYELLOW_EX}Test '{failure.test_name}' at line {failure.line_number}:{Back.RESET}")
+            print(f"  Expected: {Back.LIGHTGREEN_EX}{failure.expected_value}{Back.RESET}")
+            print(f"  Actual:   {Back.YELLOW}{failure.actual_value}{Back.RESET}")
